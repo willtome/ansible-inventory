9c132c6204d5df2900f36487cd2967944309ddc7
Alan Rominger <arominge@redhat.com>
2019-09-17 12:15:35 -0400

    make basic example even more basic (#3)

--- inventory_plugins/basic.py
+++ inventory_plugins/basic.py
@@ -9,11 +9,6 @@ DOCUMENTATION = r'''
     description:
         - Demonstrates basics of a custom inventory plugin.
     options:
-        host_base:
-            description: The base of host names.
-            type: string
-            default: host_
-            required: False
         count:
             description: The number of hosts and groups to make.
             type: integer
@@ -48,29 +43,26 @@ from base64 import b64encode
 import json
 
 
+def _hash_password(password):
+    return str(b64encode(bytes(password, encoding='utf8')), encoding='utf8')
+
+
 class InventoryModule(BaseInventoryPlugin):
 
     NAME = 'basic'
 
-    def verify_file(self, path):  # Plugin interface (1)
-        super(InventoryModule, self).verify_file(path)
-        return path.endswith(('basic.yml', 'basic.yaml'))
-
     def parse(self, inventory, loader, path, cache=True):  # Plugin interface (2)
         super(InventoryModule, self).parse(inventory, loader, path)
         self._read_config_data(path)
-        base_name = self.get_option('host_base')
-        count = self.get_option('count')
 
         root_group_name = self.inventory.add_group('root-group')  # Inventory interface (3)
 
-        for i in range(count):
-            group_name = self.inventory.add_group('{}group_{}'.format(base_name, count))  # Inventory interface (3)
+        for i in range(self.get_option('count')):
+            group_name = self.inventory.add_group('group_%s' % i)  # Inventory interface (3)
             self.inventory.add_child(root_group_name, group_name)  # Inventory interface (5)
-            host_name = self.inventory.add_host('{}{}'.format(base_name, count))  # Inventory interface (1)
+            host_name = self.inventory.add_host('host_%s' % i)  # Inventory interface (1)
             self.inventory.add_child(group_name, host_name)  # Inventory interface (4)
 
         self.inventory.set_variable(
-            root_group_name, 'hashed_password',
-            str(b64encode(bytes(self.get_option('password'), encoding='utf8')), encoding='utf8')
+            root_group_name, 'hashed_password', _hash_password(self.get_option('password'))
         )  # inventory interface (2)
